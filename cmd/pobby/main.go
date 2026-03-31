543e12a1bfc26c7fe246166052501e043761a1f4
stephanie <stephanieha2021@gmail.com>
2026-03-31 19:38:48 -0400

    initialized zone manager

--- cmd/pobby/main.go
+++ cmd/pobby/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"github.com/bettaburger/pobby/internal/command"
+
+	zone "github.com/lrstanley/bubblezone/v2"
 )
 
 func main() {
+	zone.NewGlobal()
 	command.Execute()
 }
